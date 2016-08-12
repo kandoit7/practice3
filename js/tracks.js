6384194727b5d4829047680c60cb07a01ab3520d
kandoit7 <jmin8884@gmail.com>
2016-08-12 15:34:22 +0900

    Update tracks.js

--- js/tracks.js
+++ js/tracks.js
@@ -99,6 +99,9 @@ function Track( url, left ) {
 	var pbrSlider = document.createElement("input");
 	pbrSlider.id = "scratch";
 	pbrSlider.className = "slider";
+	pbrSlider.onChange = function(event) {
+		this.parentNode.track.changePlaybackRate(event.target.value);
+	};
 	pbrSlider.type = "range";
 	pbrSlider.min = "-2";
 	pbrSlider.max = "2";
