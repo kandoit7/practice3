353c98fc1f30aaa371554c4746468eb98721f83a
kandoit7 <jmin8884@gmail.com>
2016-08-12 11:27:40 +0900

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -17,6 +17,7 @@ window.onkeypress = function(e) {
 };
 
 var controller = new Leap.Controller({enableGestures: true});
+var scratch = document.getElementById('rate');
 controller.loop(function(frame) {
 	latestFrame = frame;
 	if (paused) {
@@ -63,7 +64,10 @@ controller.loop(function(frame) {
 		// DJ Scratch
 		//
 		if( hand.yaw() < -0.7 ) {
-
+			scratch.value -= 0.1;
+		}	
+		if( hand.yaw() > 0.7 ) {
+			scratch.value += 0.1;
 		}	
 		
 	}
